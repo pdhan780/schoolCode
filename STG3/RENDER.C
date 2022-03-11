c22ad29b7d0bb9edadf0bb5139375a452529a732
pdhan780 <100167525+pdhan780@users.noreply.github.com>
2022-03-11 13:28:34 -0700

    Update RENDER.C

--- STG3/RENDER.C
+++ STG3/RENDER.C
@@ -1,9 +1,18 @@
 #include "render.h"
 
 
-void render(struct Model *model, UINT16 *base, UINT16 *ship, UINT16 *asteroid) {
 
- render_spaceship( &model->gameShip,base,ship); 
+
+
+
+void render(struct Model *model, UINT16 *base, UINT16 *ship, UINT16 *asteroid,
+            int colLevel, int rowLevel) 
+
+{
+
+ render_spaceship(&model->gameShip,base,ship); 
+
+ render_fleet (model,base,asteroid, colLevel, rowLevel);
 
  
 }
@@ -18,11 +27,52 @@ void render_spaceship(const struct Spaceship *shipPtr, UINT16 *base, UINT16 *shi
 
 
 
-void render_asteroid(const struct Asteroid *astPtr, UINT16 *base, UINT16 *asteroid)
+void render_asteroid(const struct Asteroid *astPtr, UINT16 *base,
+                     UINT16 *asteroid)
 {
 
  plot_bitmap_16 (base, astPtr->x, astPtr->y, asteroid, astPtr->height);
 
 }
+
+
+
+
+
+void render_fleet(struct Model *model, UINT16 *base, UINT16 *asteroid,
+                  int colLevel, int rowLevel)
+                   
+{
+
+
+
+/*row levels 0-9*/
+									````	/*assume rowLevel to be a constant number*/
+/*process for the row given, and plot the amount of asteroids*/
+
+
+
+if (rowLevel < 0 && rowLevel <=9) {   
+
+
+    int x;
+
+    for (x = 0; x < colLevel; x++)
+  {
+
+
+   render_asteroid(model->asteroids[rowLevel][x], base, asteroid);
+
+
+  }
+
+
+ }
+
+
+
+
+}
+
 
 
